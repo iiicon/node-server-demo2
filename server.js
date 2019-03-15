902b546e26105448c805ed5059407fe367c6e40b
shiguangwei <shigw5420@joyowo.com>
2019-03-15 23:46:30 +0800

    form post data

---
+++ server.js
@@ -0,0 +1,56 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if (!port) {
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response) {
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url
+  var queryString = ''
+  if (pathWithQuery.indexOf('?') >= 0) {
+    queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'))
+  }
+  var path = parsedUrl.pathname
+  console.log('path is '+path)
+  var query = parsedUrl.query
+  var method = request.method
+
+  console.log('含查询字符串的路径\n' + pathWithQuery)
+
+  if (path === '/') {
+    var string = fs.readFileSync('./index.html', 'utf8')
+    var amount = fs.readFileSync('./db', 'utf8')
+    string = string.replace('%%amount%%', amount)
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(string)
+    response.end()
+  } else if (path === '/style') {
+    response.setHeader('Content-Type', 'text/css;charset=utf-8')
+    response.write('* {color: #a37654;}')
+    response.end()
+  } else if (path === '/main.js') {
+    response.setHeader('Content-Type', 'text/javascript;charset=utf-8')
+    response.write('alert("test")')
+    response.end()
+  } else if(method.toUpperCase() === 'POST' && path === '/pay') {
+    var amount = fs.readFileSync('./db', 'utf8')
+    var newAmount = amount -1
+    fs.writeFileSync('./db', newAmount, 'utf8')
+    response.write('success')
+    response.end()
+  }
+  else {
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write('Not Found')
+    response.end()
+  }
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n http://localhost:' + port)
